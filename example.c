11334ae35db5a691bed730cd28ce9edb1405f939
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2025-11-03 15:37:44 +0100

    add: help command

--- example.c
+++ example.c
@@ -24,27 +24,29 @@ int main(int argc, const char** argv) {
         "This is an example."
     );
 
+    cmd_add_help_subcmd(main);
+
     arg input = cmd_arg(main, "input");
     arg_help (input, "input file");
     arg_value(input, &args.input, arg_str);
 
     arg out = cmd_arg(main, "output");
     arg_help (out, "output file");
-    arg_usage(out, "<FILE>");
+    arg_usage(out, "<file>");
     arg_short(out, 'o');
     arg_long (out, "output");
     arg_value(out, &args.output, arg_str);
 
     arg i = cmd_arg(main, "i");
     arg_help (i, "some integer");
-    arg_usage(i, "<INTEGER>");
+    arg_usage(i, "<integer>");
     arg_short(i, 'i');
     arg_check(i, &args.has_i);
     arg_value(i, &args.i, arg_int);
 
     arg f = cmd_arg(main, "f");
     arg_help (f, "some float");
-    arg_usage(f, "<FLOAT>");
+    arg_usage(f, "<float>");
     arg_short(f, 'f');
     arg_check(f, &args.has_f);
     arg_value(f, &args.f, arg_float);
